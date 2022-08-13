7e3aea1f7ee56890175c0735c83c7664c52ce175
josuefuentesdev <68027721+josuefuentesdev@users.noreply.github.com>
2022-08-13 18:06:10 -0500

    +serilog

--- Ftl.Backoffice.Presentation/Program.cs
+++ Ftl.Backoffice.Presentation/Program.cs
@@ -4,39 +4,60 @@ using Ftl.Backoffice.Application;
 using Ftl.Backoffice.DataAccess;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.Identity.Web;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+//create the logger and setup your sinks, filters and properties
+Log.Logger = new LoggerConfiguration()
+    .WriteTo.Console()
+    .CreateBootstrapLogger();
 
-builder.Services.AddApplicationServices();
-builder.Services.AddDataAccessServices(builder.Configuration);
-builder.Services.AddApiServices();
+Log.Information("Starting up");
 
-// Add services to the container.
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
-
-builder.Services.AddControllers()
-    .AddFluentValidation();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
-    c.CustomOperationIds(e => $"{e.ActionDescriptor.RouteValues["action"]}")
-);
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    var builder = WebApplication.CreateBuilder(args);
+    builder.Host.UseSerilog();
+
+    builder.Services.AddApplicationServices();
+    builder.Services.AddDataAccessServices(builder.Configuration);
+    builder.Services.AddApiServices();
+
+    // Add services to the container.
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+    builder.Services.AddControllers()
+        .AddFluentValidation();
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen(c =>
+        c.CustomOperationIds(e => $"{e.ActionDescriptor.RouteValues["action"]}")
+    );
+
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthentication();
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+catch (Exception ex)
+{
+    Log.Fatal(ex, "Unhandled exception");
+}
+finally
+{
+    Log.Information("Shut down complete");
+    Log.CloseAndFlush();
+}
